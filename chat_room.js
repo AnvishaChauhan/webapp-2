a84f579fa46e27bc58dd9d36c79a012a116917a8
AnvishaChauhan <115411141+AnvishaChauhan@users.noreply.github.com>
2023-02-19 19:21:45 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCbyzqb-iZYu6T3Ct_KB4xgq5a12hZGhkM",
+    authDomain: "kendriya-vidyalaya-hamirpur.firebaseapp.com",
+    databaseURL: "https://kendriya-vidyalaya-hamirpur-default-rtdb.firebaseio.com",
+    projectId: "kendriya-vidyalaya-hamirpur",
+    storageBucket: "kendriya-vidyalaya-hamirpur.appspot.com",
+    messagingSenderId: "29652148581",
+    appId: "1:29652148581:web:6bd64c5327baed6aed018c"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS HERE
+
